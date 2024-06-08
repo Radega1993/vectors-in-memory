95224d0d43e1bc33f5cdf6425f07918dd8d646e0
Raul de Arriba <rauldearriba@gmail.com>
2024-06-08 12:54:08 +0200

    end vectorsdb in memory

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain_community.vectorstores import FAISS
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("hi")
+    pdf_path = '/home/raul/Proyectos/vectors-in-memory/paper.pdf'
+    loader = PyPDFLoader(file_path=pdf_path)
+    document = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents=document)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("faiss_index_react")
+
+    new_vectorstore = FAISS.load_local(
+        "faiss_index_react", embeddings, allow_dangerous_deserialization=True
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        OpenAI(), retrieval_qa_chat_prompt
+    )
+    retrieval_chain = create_retrieval_chain(
+        new_vectorstore.as_retriever(), combine_docs_chain
+    )
+
+    res = retrieval_chain.invoke({"input": "Give me the gist of ReAct in 3 sentences"})
+    print(res["answer"])
